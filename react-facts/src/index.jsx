cd11d5a3741e5b4604ca2985b22a0038092d206d
MESHAL OKAIRY <MOKAIRY@users.noreply.github.com>
2025-09-23 01:54:24 +0300

    Create App component

--- react-facts/src/index.jsx
+++ react-facts/src/index.jsx
@@ -1,18 +1,6 @@
 import { createRoot } from "react-dom/client";
-import { Header } from "./components/Header";
-import { MainContent } from "./components/MainContent";
-import { Footer } from "./components/Footer";
+import { App } from "./App";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(<Page />);
-
-function Page() {
-  return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
-  );
-}
+root.render(<App />);
