74eb5f2452b0fbcefe293f290bcef7286460cdd0
MESHAL OKAIRY <MOKAIRY@users.noreply.github.com>
2025-09-23 02:29:42 +0300

    Modify CSS style

--- react-facts/src/App.jsx
+++ react-facts/src/App.jsx
@@ -1,13 +1,11 @@
-import { Header } from "./components/Header";
+import { Navbar } from "./components/Navbar";
 import { MainContent } from "./components/MainContent";
-import { Footer } from "./components/Footer";
 
 export function App() {
   return (
     <>
-      <Header />
+      <Navbar />
       <MainContent />
-      <Footer />
     </>
   );
 }
--- react-facts/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-  return (
-    <footer>
-      <small>© 2025 MOKAIRY development. All rights reserved.</small>
-    </footer>
-  );
-}
