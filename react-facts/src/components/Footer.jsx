e9d44506b13bd9336a52da028cab844e371ae80a
MESHAL OKAIRY <MOKAIRY@users.noreply.github.com>
2025-09-23 01:06:51 +0300

    Separate each component into different file

---
+++ react-facts/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer>
+      <small>© 2025 MOKAIRY development. All rights reserved.</small>
+    </footer>
+  );
+}
--- react-facts/src/index.jsx
+++ react-facts/src/index.jsx
@@ -1,37 +1,18 @@
 import { createRoot } from "react-dom/client";
-import logo from "./assets/react-logo.png";
-
-function MainContent() {
-  return <h1>React is great 1</h1>;
-}
+import { Header } from "./components/Header";
+import { MainContent } from "./components/MainContent";
+import { Footer } from "./components/Footer";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(
-  <div>
-    <Main />
-  </div>
-);
+root.render(<Page />);
 
-function Main() {
+function Page() {
   return (
-    <div>
-      <header>
-        <img src={logo} alt="React Logo" width="40px" />
-      </header>
-      <main>
-        <h1>Fun facts about React!</h1>
-        <ul>
-          <li>Was first release in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 200K stars on GitHub</li>
-          <li>Is maintained by Meta</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </main>
-      <footer>
-        <small>© 2025 MOKAIRY development. All rights reserved.</small>
-      </footer>
-    </div>
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
   );
 }
