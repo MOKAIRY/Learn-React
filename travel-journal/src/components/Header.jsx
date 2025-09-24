c53118594489ba4f50f386cfb0cfb7321e4d09d3
MESHAL OKAIRY <MOKAIRY@users.noreply.github.com>
2025-09-24 21:38:31 +0300

    Create first entry

--- travel-journal/src/components/Header.jsx
+++ travel-journal/src/components/Header.jsx
@@ -2,11 +2,9 @@ import Logo from "../assets/globe.png";
 
 export default function Header() {
   return (
-    <>
-      <header>
-        <img src={Logo} alt="Logo" />
-        <h1>My Travel journal</h1>
-      </header>
-    </>
+    <header>
+      <img src={Logo} alt="Logo" />
+      <h1>My Travel journal</h1>
+    </header>
   );
 }
