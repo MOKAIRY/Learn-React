fce3dca5ff777cd4b9abc5c81d12c7c561ab09d8
MESHAL OKAIRY <MOKAIRY@users.noreply.github.com>
2025-09-26 04:07:52 +0300

    Used .map() to simplify the code

--- travel-journal/src/components/Entry.jsx
+++ travel-journal/src/components/Entry.jsx
@@ -1,19 +1,19 @@
-import JapanImg from "../assets/Japan.jpg";
 import Marker from "../assets/marker.png";
 
 export default function Entry(props) {
+  console.log(props);
   return (
     <article className="journal-entry">
       <div className="main-image-container">
-        <img className="main-image" src={props.img} alt="mount fuji" />
+        <img className="main-image" src={props.img.src} alt={props.img.alt} />
       </div>
       <div className="info-container">
         <img className="marker" src={Marker} alt="map marker icon" />
-        <span className="country">{props.name}</span>
+        <span className="country">{props.country}</span>
         <a href={props.link}>View on Google Maps</a>
         <h2 className="entry-title">{props.title}</h2>
         <p className="trip-dates">{props.date}</p>
-        <p className="entry-text">{props.description}</p>
+        <p className="entry-text">{props.text}</p>
       </div>
     </article>
   );
